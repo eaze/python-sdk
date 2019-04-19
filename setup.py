a7006442896492576716284cf9b5d1a1d39fc1d4
fredhope2000 <fredhope2000@users.noreply.github.com>
2019-04-19 01:13:02 -0700

    0.1.0

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from setuptools import setup, find_packages
 
 setup(name='shift_planning',
-      version='0.0.1',
+      version='0.1.0',
       description='Shiftplanning API',
       url='https://www.shiftplanning.com/api/',
       packages=find_packages(),
